19e25a0608b73872105500c77cf0e72383872faf
Justin Farnsworth <farnswj1@tcnj.edu>
2022-02-06 16:02:46 -0500

    refactored, fixed spacing/tab inconsistency

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 
 const Footer = () => (
-	<Box sx={{ p: 2, mt: 'auto' }}>
-		<Typography variant="h6" align="center">
-			&copy; farnswj1 2022
-		</Typography>
-	</Box>
+  <Box sx={{ p: 2, mt: 'auto' }}>
+    <Typography variant="h6" align="center">
+      &copy; farnswj1 2022
+    </Typography>
+  </Box>
 );
 
 export default Footer;
--- frontend/src/components/PrimalityTestForm/PrimalityTestForm.jsx
+++ frontend/src/components/PrimalityTestForm/PrimalityTestForm.jsx
@@ -2,47 +2,47 @@ import React, { useState } from 'react';
 import { Paper, Box, Typography, FormControl, TextField, Button } from '@mui/material';
 
 const PrimalityTestForm = ({ handleSubmit }) => {
-	const [error, setError] = useState(false);
-	const [formChanged, setFormChanged] = useState(false);
+  const [error, setError] = useState(false);
+  const [formChanged, setFormChanged] = useState(false);
 
-	const handleNumberChange = (event) => {
-		const number = event.target.value.trim();
-		const intRegex = /^([2-9]|[1-9][0-9]+)$/;
-		setError(!intRegex.test(number));
+  const handleNumberChange = (event) => {
+    const number = event.target.value.trim();
+    const intRegex = /^([2-9]|[1-9][0-9]+)$/;
+    setError(!intRegex.test(number));
 
-		if (!formChanged) {
-			setFormChanged(true);
-		}
-	};
+    if (!formChanged) {
+      setFormChanged(true);
+    }
+  };
 
-	return (
-		<Paper square={false} sx={{ p: 2 }}>
-			<Typography variant="h4">Is It Prime?</Typography>
-			<Box component="form" onSubmit={handleSubmit}>
-				<FormControl fullWidth variant="outlined">
-					<TextField
-						id="number"
-						label="Enter an integer greater than 1"
-						multiline={true}
-						rows={8}
-						sx={{ my: 3 }}
-						required
-						InputLabelProps={{ required: false }}
-						onChange={handleNumberChange}
-						error={error}
-					/>
-				</FormControl>
-				<Button
-					variant="contained"
-					type="submit"
-					size="large"
-					disabled={error || !formChanged}
-				>
-					Submit
-				</Button>
-			</Box>
-		</Paper>
-	);
+  return (
+    <Paper square={false} sx={{ p: 2 }}>
+      <Typography variant="h4">Is It Prime?</Typography>
+      <Box component="form" onSubmit={handleSubmit}>
+        <FormControl fullWidth variant="outlined">
+          <TextField
+            id="number"
+            label="Enter an integer greater than 1"
+            multiline={true}
+            rows={8}
+            sx={{ my: 3 }}
+            required
+            InputLabelProps={{ required: false }}
+            onChange={handleNumberChange}
+            error={error}
+          />
+        </FormControl>
+        <Button
+          variant="contained"
+          type="submit"
+          size="large"
+          disabled={error || !formChanged}
+        >
+          Submit
+        </Button>
+      </Box>
+    </Paper>
+  );
 }
 
 export default PrimalityTestForm;
