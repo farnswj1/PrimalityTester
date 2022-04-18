8fa03fe74e49ec6eec0e7e8bdfef5a24daa90123
Justin Farnsworth <farnswj1@tcnj.edu>
2022-04-18 10:15:45 -0400

    more optimizations

--- backend/backend/core/decorators.py
+++ backend/backend/core/decorators.py
@@ -5,19 +5,19 @@ from urllib.parse import quote
 def memoize(timeout=None):
     def decorator_func(func):
         def _get_cache_key(*args, **kwargs):
-            func_name = func.__name__
-            args_str = '_'.join(quote(str(arg)) for arg in args)
-            kwargs_str = '_'.join(
-                f'{quote(key)}={quote(str(value))}' for key, value in sorted(kwargs.items())
-            )
-            
-            cache_key = func_name
+            cache_key = func.__name__
 
-            if args_str:
+            if args:
+                args_str = '_'.join(quote(str(arg)) for arg in args)
                 cache_key += f'__{args_str}'
-            if kwargs_str:
+
+            if kwargs:
+                kwargs_str = '_'.join(
+                    f'{quote(key)}={quote(str(value))}'
+                    for key, value in sorted(kwargs.items())
+                )
                 cache_key += f'__{kwargs_str}'
-            
+
             return cache_key
 
         def wrapper(*args, **kwargs):
