7dd3e16722e2bb5a04dce948eafa06b957458b9a
Justin Farnsworth <farnswj1@tcnj.edu>
2022-08-17 21:47:32 -0400

    changed a test to validate an IP address

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -8,6 +8,7 @@ class TestIPAddress(APITestCase):
         self.ip_address = IPAddressFactory()
 
     def test_ip_address_created(self):
-        self.assertIsInstance(self.ip_address.ip_address, str)
+        octets = tuple(int(octet) for octet in self.ip_address.ip_address.split('.'))
+        self.assertTrue(all(0 <= octet <= 255 for octet in octets))
         self.assertIsInstance(self.ip_address.date_added, datetime)
         self.assertIsInstance(self.ip_address.last_request, datetime)
