4ebbeaa1abceb8f899ce4bc0cc52ce57f6d9d3e5
Justin Farnsworth <farnswj1@tcnj.edu>
2023-10-17 10:42:16 -0400

    removed unnecessary frontend deps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,15 +2,9 @@ import { defineConfig } from 'vitest/config';
 import { createHtmlPlugin } from 'vite-plugin-html';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, 'src')
-    }
-  },
   plugins: [
     createHtmlPlugin({ minify: true, entry: 'src/index.tsx' }),
     react(),
