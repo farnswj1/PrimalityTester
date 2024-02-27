3bd06d9358401349a4b731b40535d416472b8207
Justin Farnsworth <farnswj1@tcnj.edu>
2024-02-26 22:03:57 -0500

    add logging to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import morgan from 'morgan';
 import { RateLimiter } from 'middleware';
 import Router from 'routes';
 import { PORT } from 'settings';
@@ -9,6 +10,7 @@ const app: Express = express();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(morgan('combined'));
 app.use(RateLimiter({ endpoint: 'api', time: 60, limit: 5 }));
 app.use('/api', Router);
 app.set('trust proxy', true);
