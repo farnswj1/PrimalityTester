9ce09c0361fd6412ccde121b1c0135e96a50ff54
Justin Farnsworth <farnswj1@tcnj.edu>
2024-03-23 12:00:49 -0400

    removed tabs in code

--- backend/src/libs/bigmath.ts
+++ backend/src/libs/bigmath.ts
@@ -1,33 +1,33 @@
 export const sqrt = (value: bigint): bigint => {
-	if (value < 0n) {
-		throw new Error('Square root of negative numbers is not supported!');
-	} else if (value < 2n) {
-		return value;
-	} else if (value < 16n) {
-		return BigInt(Math.sqrt(Number(value)) | 0);
-	}
+  if (value < 0n) {
+    throw new Error('Square root of negative numbers is not supported!');
+  } else if (value < 2n) {
+    return value;
+  } else if (value < 16n) {
+    return BigInt(Math.sqrt(Number(value)) | 0);
+  }
 
   let x0: bigint;
   let x1: bigint;
 
   if (value < 4503599627370496n) { // 1n << 52n
-		x1 = BigInt(Math.sqrt(Number(value)) | 0) - 3n;
-	} else {
-		const length = value.toString().length;
+    x1 = BigInt(Math.sqrt(Number(value)) | 0) - 3n;
+  } else {
+    const length = value.toString().length;
 
     if (!(length & 1)) {
-			x1 = 10n ** BigInt(length / 2);
-		} else {
-			x1 = 4n * 10n ** BigInt((length / 2) | 0);
-		}
-	}
+      x1 = 10n ** BigInt(length / 2);
+    } else {
+      x1 = 4n * 10n ** BigInt((length / 2) | 0);
+    }
+  }
 
-	do {
-		x0 = x1;
-		x1 = ((value / x0) + x0) >> 1n;
-	} while (x0 !== x1 && x0 !== (x1 - 1n));
+  do {
+    x0 = x1;
+    x1 = ((value / x0) + x0) >> 1n;
+  } while (x0 !== x1 && x0 !== (x1 - 1n));
 
-	return x0;
+  return x0;
 };
 
 export const modPow = (base: bigint, exp: bigint, mod: bigint): bigint => {
